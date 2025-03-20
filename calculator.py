7e69648cbaea9ccbfb1bbf84fa8903d9ccc4d844
SHANKI DEVADIGA <142487745+ShankiDevadiga@users.noreply.github.com>
2025-03-20 11:56:54 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,15 +5,5 @@ def multiplication(a, b):
 def square(n):
     return n ** 2
 
-def addition(a, b):
-    return a + b
 
-def division(a, b):
-    if b != 0:
-        return a / b
-    else:
-        return "Cannot divide by zero"
-
-def difference(a, b):
-    return a - b
 
